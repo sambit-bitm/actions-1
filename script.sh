45b85eef27af435e80d9c55316f8fdb6566a4991
sambit-bitm <34181115+sambit-bitm@users.noreply.github.com>
2024-08-31 10:09:57 +0530

    check env

--- script.sh
+++ script.sh
@@ -4,4 +4,6 @@ sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover , I am a DRAGON ...RAWR" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
+cat dragon.txt >> $GITHUB_ENV
 ls -ltra
+cat $GITHUB_ENV
